17223c9292af42b21b1e7693898b548d1bbe2739
Elvinas Predkelis <elvinas.predkelis@gmail.com>
2024-08-14 10:22:11 +0300

    scrollto controller

--- src/scrollto.js
+++ src/scrollto.js
@@ -1,18 +1,32 @@
 import { Controller } from "@hotwired/stimulus";
 
+// This controller is used to scroll to target or an anchor
+
+// USAGE
+// Connects to data-controller="scrollto"
+//
+// EXAMPLE
+// <a href="#target" data-controller="scrollto">Scroll to anchor</a>
+// <button data-controller="scrollto" data-scrollto-selector-value="#target_identifier">Scroll to target</button>
+
 export default class extends Controller {
-  static targets = ["target"];
+  static values = {
+    selector: String,
+    scrollOptions: {
+      type: Object,
+      default: {
+        behavior: "smooth",
+      },
+    },
+  };
 
   scroll(event) {
     event.preventDefault();
 
     const anchor = this.element.getAttribute("href");
-    const target = this.hasTargetTarget
-      ? this.targetTarget
-      : document.querySelector(anchor);
+    const selector = this.hasSelectorValue ? this.selectorValue : anchor;
+    const target = document.querySelector(selector);
 
-    target.scrollIntoView({
-      behavior: "smooth",
-    });
+    if (!!target) target.scrollIntoView(this.scrollOptionsValue);
   }
 }
